3be6ca947c699d84ccc13872fda7e93133472408
oliver <896986794@qq.com>
2020-04-16 23:38:33 +0800

    try

---
+++ app.js
@@ -0,0 +1,8 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+ScrollReveal().reveal('.widget', { interval: 200 ,reset:true,distance: '70px'});
+ScrollReveal().reveal('.skills', { interval: 2000 ,reset:true,distance: '70px'});
+ScrollReveal().reveal('.portfolio', {interval: 2000 ,reset:true,distance: '70px'});
+ScrollReveal().reveal('.message', { interval: 2000 ,reset:true,distance: '70px'});
